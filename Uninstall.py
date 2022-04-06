91241a35eb40e1c65392aa3972ce7a59e8bdc1cc
unknown <nguyenxuanhoang02042001@gmail.com>
2022-04-06 22:02:09 +0700

    hoang01

--- Uninstall.py
+++ Uninstall.py
@@ -1,14 +1,16 @@
-from os import system,environ
-from dotenv import load_dotenv
-load_dotenv(dotenv_path='Driver/.env')
+from os import system
 def Delete():
-    password=environ.get('username')
+    password='123'
     print('You will remove all the code :')
-    password1=input('You Name :')
+    password1=input('You Password :')
     if password==password1:
-        system('cd ..')
-        system('rmdir /q /s COPYCODEPTIT1')
-        print()
+        system('del .gitignore')
+        system('del  main.py')
+        system('del  README.md')
+        system('del  Libary.py')
+        system('del  rmdir /q /s Driver')
+        system('del  rmdir /q /s __pycache__')
+        system('del  Uninstall.py')
     else :
         print('Wrong Password')
 Delete()
