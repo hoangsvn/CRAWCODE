8bda22b6935c0d3574eb9d451582b6a5a40df61c
unknown <nguyenxuanhoang02042001@gmail.com>
2022-04-06 21:51:53 +0700

    hoang

--- Uninstall.py
+++ Uninstall.py
@@ -1,10 +1,14 @@
-from os import system
-def piplist():
-    list=system('pip list')
-    return list
+from os import system,environ
+from dotenv import load_dotenv
+load_dotenv(dotenv_path='Driver/.env')
 def Delete():
-    piplist()
-    A=input('Nhap ten can xoa :').lower()
-    S=f'pip uninstall {A} -y'
-    system(S)
+    password=environ.get('username')
+    print('You will remove all the code :')
+    password1=input('You Name :')
+    if password==password1:
+        system('cd ..')
+        system('rmdir /q /s COPYCODEPTIT1')
+        print()
+    else :
+        print('Wrong Password')
 Delete()
