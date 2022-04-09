adb241c02008601bd1dc98c864368fdd9fd5e8ad
unknown <nguyenxuanhoang02042001@gmail.com>
2022-04-09 22:41:41 +0700

    hoang

--- Libary.py
+++ Libary.py
@@ -3,24 +3,16 @@ from datetime import datetime
 from dotenv import load_dotenv
 
 
-load_dotenv(dotenv_path='Driver/.env')
+load_dotenv(dotenv_path='.env')
 Save_Code = int(environ.get('Save_Code'))
 Save_History = int(environ.get('Save_History'))
 Folder_path = environ.get('Folder_path')
 profile='\n# Nguoi viet : Nguyen Xuan Hoang \n# Date : 05/04/2022\n# Email : hoangnxb19dcat079.gmail.com\n'
 endfile ='\n# Oke nha\n'
 
-def Clearcmd():
-    system('cls')
-
 
 def SaveFolder():
-   mkdir(Folder_path)
-
-
-def Delete():
-    cmd = f'rmdir /q /s {Folder_path}'
-    system(cmd)
+   system(f'md {Folder_path}')
 
 
 def GhiFileCode(name, string):
@@ -43,27 +35,5 @@ def Error(string):
         with open(path, 'a+', encoding='UTF-8') as File:
             File.write(
                 f'{string} CHƯA LÀM HOẶC LÀM SAI LƯA VÀO NGÀY {datetime.now()}\n')
-
-
-def TrangThai(a, b):
-    Clearcmd()
-    if a != b:
-        print(f'<=================[RUNING {int((a/b)*100)} % ]=================>')
-    else:
-        print(f'<=================[SAVE FOlDER DATA]=================>')
-
-def google_version():
-    stream = popen('reg query "HKLM\\SOFTWARE\\Wow6432Node\\Microsoft\\Windows\\CurrentVersion\\Uninstall\\Google Chrome"')
-    output = stream.read()
-    try:
-        google_version = ''
-        for letter in output[output.rindex('DisplayVersion    REG_SZ') + 24:]:
-            if letter != '\n':
-                google_version += letter
-            else:
-                break
-        return(google_version.strip())
-    except TypeError:
-        return
 
 
--- main.py
+++ main.py
@@ -1,80 +1,76 @@
+import os
+import time
+import threading
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
-from time import sleep
-from Libary import Clearcmd,TrangThai,SaveFolder,History,GhiFileCode,Error
 from dotenv import load_dotenv
-from os import environ
-load_dotenv(dotenv_path='Driver/.env')
-# loadDrive
-def LoadDrive():
-    return webdriver.Chrome(executable_path="Driver/chromedriver.exe")
-# KhoiChay Chrome
-
-
-def RunChrome():
-    SaveFolder()
-    Clearcmd()
-    brower.set_window_position(0,0)
-    brower.set_window_size(50,50)
-    brower.get("https://code.ptit.edu.vn")
-# LoginPTITcode
-
-
-def LoginPTIT():
-    sleep(1)
-    Clearcmd()
-    brower.find_element_by_id("login__user").send_keys(environ.get('login__user'))
+from Libary import History,GhiFileCode,Error
+load_dotenv(dotenv_path='.env')
+def LoginPTIT(brower):
+    brower.find_element_by_id("login__user").send_keys(os.environ.get('login__user'))
     password = brower.find_element_by_id("login__pw")
-    password.send_keys(environ.get('login__pw'))
+    password.send_keys(os.environ.get('login__pw'))
+    os.system('cls')
     password.send_keys(Keys.ENTER)
-    Clearcmd()
-    sleep(1)
-    if len(brower.find_elements_by_class_name("text--red")) == 1:
-        Clearcmd()
-        print("Tài khoản hoặc mật khẩu không hợp lệ.".upper())
-        return False
-    else:
-        return True
-
-
-def ListTenbai():
-    List = []
-    for i in range(1, 4):
-        brower.get("https://code.ptit.edu.vn/student/question?page="+str(i))
-        crawl = brower.find_elements_by_class_name("bg--10th")
-        Clearcmd()
-        for i in range(len(crawl)):
-            List.append(crawl[i].find_element_by_xpath("td[3]/a").text)
-    
+def Listbai(brower,x):
+    List=[]
+    brower.get("https://code.ptit.edu.vn/student/question?page="+str(x))
+    crawl = brower.find_elements_by_class_name("bg--10th")
+    for i in range(len(crawl)):
+        List.append(crawl[i].find_element_by_xpath("td[3]/a").text)
+    os.system('cls')
     return List
-
-
-def CrawlCode():
-    Count = 0
-    ListName = ListTenbai()
-    lenlist = len(ListName)
-    for Bai in ListName:
-        Count += 1
+def Crawl(brower,x,timedelay):
+    m=0
+    list=Listbai(brower,x)
+    Len=len(list)
+    for Bai in list:
+        m+=1
         brower.get("https://code.ptit.edu.vn/student/question/"+Bai)
-        sleep(1)
+        time.sleep(timedelay)
+        print(f'Thread {x} runing : {int(m*100/Len)} %')
         TAB = brower.find_elements_by_class_name("card-body")
-        TrangThai(Count, lenlist)
         if len(TAB) > 0:
             if len(TAB[0].find_elements_by_class_name("text--middle")) > 0:
                 brower.get(TAB[0].find_element_by_xpath("div[1]/table/tbody/tr[1]/td[4]/a").get_attribute("href"))
-                sleep(1)
+                time.sleep(timedelay)
                 source = brower.find_element_by_name("source_code")
                 GhiFileCode(Bai, source.get_attribute("value"))
+                os.system('cls')
                 History(brower.find_element_by_class_name("submit__nav").find_element_by_xpath("p/a").text)
             else:
                 Error(Bai)
-    sleep(1)
-    return
+    time.sleep(1)
+def Runtime (i):
+    print(f'Thread {i} runing')
+    if i==0:
+        os.system('cls')
+    else:
+        Options=webdriver.ChromeOptions()
+        driver = webdriver.Chrome(options=Options)
+        driver.set_window_size(60,300)
+        driver.set_window_position(i*100,0)
+        driver.get('https://code.ptit.edu.vn')
+        LoginPTIT(driver)
+        Crawl(driver,i,2)
+        time.sleep(1)
 
+def Thread3():
+    soluorg =4
+    threats =[]
+    for i in range(soluorg):
+        threats += [threading.Thread(target=Runtime,args={i})]
+    for t in threats:
+        t.start()
+    for t in threats:
+        t.join()
+def Thread1():
+    for i in range(1,4):
+        Runtime(i)
 
 if __name__ == '__main__':
-    brower = LoadDrive()
-    RunChrome()
-    if LoginPTIT():
-        CrawlCode()
-    brower.close()
+    A=input('Chay 3 luong hay 1 luong  yes=1/no=0 recommand = 0 :')
+    if A=='1':
+        Thread3()
+    else :
+        Thread1()
