c65062783162ef611c8de6e533df0220c884b99a
unknown <nguyenxuanhoang02042001@gmail.com>
2022-04-11 21:17:58 +0700

    hoang

--- Crawl Code Ptit With Python/main.py
+++ Crawl Code Ptit With Python/main.py
@@ -1,6 +1,6 @@
-import os
-import time
-import threading
+from os import getenv,system
+from time import sleep
+from threading import Thread
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from dotenv import load_dotenv
@@ -8,10 +8,10 @@ from Libary import History,GhiFileCode,Error,SaveFolder
 load_dotenv(dotenv_path='.env')
 def LoginPTIT(brower):
     try:
-        brower.find_element_by_id("login__user").send_keys(os.getenv('login__user'))
+        brower.find_element_by_id("login__user").send_keys(getenv('login__user'))
         password = brower.find_element_by_id("login__pw")
-        password.send_keys(os.getenv('login__pw'))
-        os.system('cls')
+        password.send_keys(getenv('login__pw'))
+        system('cls')
         password.send_keys(Keys.ENTER)
     except:
         return
@@ -22,7 +22,7 @@ def Listbai(brower,x):
         crawl = brower.find_elements_by_class_name("bg--10th")
         for i in range(len(crawl)):
             List.append(crawl[i].find_element_by_xpath("td[3]/a").text)
-        os.system('cls')
+        system('cls')
         return List
     except:
         return
@@ -34,26 +34,26 @@ def Crawl(brower,x,timedelay):
         for Bai in list:
             m+=1
             brower.get("https://code.ptit.edu.vn/student/question/"+Bai)
-            time.sleep(timedelay)
+            sleep(timedelay)
             print(f'Thread {x} runing : {int(m*100/Len)} %')
             TAB = brower.find_elements_by_class_name("card-body")
             if len(TAB) > 0:
                 if len(TAB[0].find_elements_by_class_name("text--middle")) > 0:
                     brower.get(TAB[0].find_element_by_xpath("div[1]/table/tbody/tr[1]/td[4]/a").get_attribute("href"))
-                    time.sleep(timedelay)
+                    sleep(timedelay)
                     source = brower.find_element_by_name("source_code")
                     GhiFileCode(Bai, source.get_attribute("value"))
-                    os.system('cls')
+                    system('cls')
                     History(brower.find_element_by_class_name("submit__nav").find_element_by_xpath("p/a").text)
                 else:
                     Error(Bai)
-        time.sleep(1)
+        sleep(1)
     except:
         return
 def Runtime (i):
     print(f'Thread {i} runing')
     if i==0:
-        os.system('cls')
+        system('cls')
     else:
         Options=webdriver.ChromeOptions()
         driver = webdriver.Chrome(options=Options)
@@ -62,13 +62,13 @@ def Runtime (i):
         driver.get('https://code.ptit.edu.vn')
         LoginPTIT(driver)
         Crawl(driver,i,2)
-        time.sleep(1)
+        sleep(1)
 
 def Thread3():
     soluorg =4
     threats =[]
     for i in range(soluorg):
-        threats += [threading.Thread(target=Runtime,args={i})]
+        threats += [Thread(target=Runtime,args={i})]
     for t in threats:
         t.start()
     for t in threats:
