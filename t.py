81734f7f30cdd513a66f37e8e2ca2d32b9682611
unknown <nguyenxuanhoang02042001@gmail.com>
2022-04-11 15:39:16 +0700

    hoang

--- t.py
+++ t.py
@@ -1,5 +1,9 @@
 from os import system
-system('git status')
-system('git add .')
-system('git commit -m "hoang"')
-system('git push')
+try:
+    system('git status')
+    system('git add .')
+    system('git commit -m "hoang"')
+    system('git push')
+    
+except:
+    print('ERROR')
