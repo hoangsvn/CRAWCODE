a075ce9a41c6c5e716afb95fbd81734bb8f7550f
unknown <nguyenxuanhoang02042001@gmail.com>
2022-04-12 10:40:54 +0700

    hoang

--- Crawl Truyen Mechuyenchu/main.py
+++ Crawl Truyen Mechuyenchu/main.py
@@ -4,11 +4,10 @@ import requests
 import Libary
 import html5lib
 from bs4 import BeautifulSoup as BS
-url='https://metruyenchu.com/truyen/dinh-cap-khi-van-lang-le-tu-luyen-ngan-nam'
 def Chuong():
     l=int(BS(requests.get(url).text,'html5lib').find('div',class_='font-weight-semibold h4 mb-1').text)
     return int(l*0.25),int(l*0.5),int(l*0.75),l
-x1,x2,x3,l=Chuong() 
+
 def Crawl(url,a,b):
     try:
         for j in range(a,b):
@@ -33,6 +32,7 @@ def Runtime (i):
         print(f'TOO MANY REQUESTS IN THREAD {i}')
     print(f'THREAD {i} END')
 def Thread3():   
+
     soluorg =4
     threats =[]
     for i in range(soluorg):
@@ -47,7 +47,9 @@ def Thread1():
         Runtime(i)
 if __name__ == '__main__':
     Libary.SaveFoder()
-    A = input('SPEED 3X PRESS ANY 1X PRESS 0 : ')
+    url=input('Nhap url truyen :')
+    x1,x2,x3,l=Chuong() 
+    A = input('SPEED 4X PRESS ANY 1X PRESS 0 : ')
     if A != '1':
         print('SPEED 3X')
         Thread3()
