7196db35a42fb9ef557dc1b1def403ed73b99684
unknown <nguyenxuanhoang02042001@gmail.com>
2022-04-12 20:44:51 +0700

    hoang

--- gp.py
+++ gp.py
@@ -1,9 +1,13 @@
 from os import system
-try:
-    system('git status')
-    system('git add .')
-    A=input('Nhap commit :').strip()
-    system(f'git commit -m "{A}"')
-    system('git push')
-except:
-    print('ERROR ==> git pull')
+def Run():
+    try:
+        system('git status')
+        system('git add .')
+        A=input('Nhap commit :').strip()
+        system(f'git commit -m "{A}"')
+        system('git push')
+    except:
+        print('ERROR ==> git pull')
+        system('git pull')
+        Run()
+Run()
