ec8ef4a710b360ea10afd03cecfa47e634d2b158
unknown <nguyenxuanhoang02042001@gmail.com>
2022-04-14 11:08:11 +0700

    hoang

--- Crawl Truyen Mechuyenchu/main.py
+++ Crawl Truyen Mechuyenchu/main.py
@@ -1,58 +1,51 @@
 import threading,time,requests,Libary, html5lib
 from bs4 import BeautifulSoup as BS
-soluorg =4
-def Chuong():
+
+def Chuong(th):
     try:
+        li=[]
         l=int(BS(requests.get(url).text,'html5lib').find('div',class_='font-weight-semibold h4 mb-1').text)
-        return int(l*0.25),int(l*0.5),int(l*0.75),l
+        for i in range(th):
+            li.append(int((l*i)/(th-1)))
+        time.sleep(1)
+        return li
     except:
         print('URL KHONG TON TAI')
 
-def Crawl(url,a,b):
+def Crawl(url,i):
     ten=str(url).split("/")[4]
     try:
-        for j in range(a,b):
-            soup=BS(requests.get(f'{url}/chuong-{j}').text,'html5lib')
-            text=str(soup.find(id='js-read__content').text)
-            Libary.GhiFileCode(f'{ten}-chuong{j}',text)
-    except:
-        print('TOO MANY REQUESTS IN ')
-def Runtime (i):
-    print(f'THREAD {i} RUNING')
-    try:
-        if i%4==0:
-            time.sleep(i)
-            Crawl(url,1,x1)
-        elif i%4==1 :
-            time.sleep(i)
-            Crawl(url,x1,x2)
-        elif i%4==2 :
-            time.sleep(i)
-            Crawl(url,x2,x3)
-        elif i%4==3 :
-            time.sleep(i)
-            Crawl(url,x3,l+1)
-        time.sleep(1)
+        for j in range(list[i-1],list[i]):
+            try:
+                soup=BS(requests.get(f'{url}/chuong-{j}').text,'html5lib')
+                text=str(soup.find(id='js-read__content').text)
+                Libary.GhiFileCode(f'{ten}-chuong-{j}',text)
+            except:
+                continue
     except:
         print(f'TOO MANY REQUESTS IN THREAD {i}')
-    print(f'THREAD {i} END')
-def Thread3():   
+
+def Runtime (i):
+    # time.sleep(i)
+    Crawl(url,i)
+
+
+def Thread(th):   
     threats =[]
-    for i in range(soluorg):
+    for i in range(1,th):
         threats += [threading.Thread(target=Runtime,args={i})]
     for t in threats:
         t.start()
     for t in threats:
         t.join()
     print('THREAD END')
-def Thread1():
-    for i in range(1,4):
-        Runtime(i)
+
 if __name__ == '__main__':
     Libary.SaveFoder()
     try:
-        url=input('NHAP URL TRUYEN MECHUYENCHU.COM :')
-        x1,x2,x3,l=Chuong() 
-        Thread3()
+        url=input('NHAP URL TRUYEN MECHUYENCHU.COM :') 
+        th=int('NHAP SO LUONG CHAY  0 < : < 50 : ') or 25
+        list=Chuong(th)
+        Thread(th)
     except:
         print(f'ERROR : {url}')
