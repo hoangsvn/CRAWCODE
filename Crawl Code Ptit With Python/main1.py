07f35fe9871726905e4d692a2c027d7a09eae0a9
unknown <nguyenxuanhoang02042001@gmail.com>
2022-04-14 10:01:49 +0700

    hoang

--- Crawl Code Ptit With Python/main1.py
+++ Crawl Code Ptit With Python/main1.py
@@ -26,6 +26,16 @@ def Login():
         print('ERROR NO INTERNET CONNECTION')
     return log,s
 
+def Munpage(s):
+    m=0
+    for i in range(1,10):
+        ac=s.get("https://code.ptit.edu.vn/student/question?page="+str(i))
+        sleep(0.8)
+        list=BS(ac.content,'html.parser').find('tbody').find_all('tr')
+        if len(list)>0:
+            m=m+1
+    return m+1
+
 
 def CrawlCode(x,a):
     try:
@@ -55,11 +65,11 @@ def Runtime(i,S):
         CrawlCode(i,S)
     except:
         return
-soluorg = 4
 
-def Thread3(S):
+
+def Thread3(soluong,S):
     threats = []
-    for i in range(1,soluorg):
+    for i in range(1,soluong):
         threats += [Thread(target=Runtime, args={i,S})]
     for t in threats:
         t.start()
@@ -70,11 +80,12 @@ def Thread3(S):
 if __name__ == '__main__':
     print('COPY CODE AC TREN CODE PTIT VE MAY ')
     T,S=Login()
+    
     if T :
         Folder=SaveFolder()
-        print('LOGIN SUCCESSUFUL')
-        print('RUNING ')
-        Thread3(S)
+        print('LOGIN SUCCESSUFUL AND RUNING')
+        soluong=Munpage(S)
+        Thread3(soluong,S)
         print(f'DATACODE IN {Folder} ')
         sleep(5)
     else :
